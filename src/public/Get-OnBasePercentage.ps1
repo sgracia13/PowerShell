534479748ff98993c2d02399d6c22ba535a0f664
rgracia <rgracia@netchexonline.com>
2020-04-05 12:26:18 -0500

    add synopsis section. still modifying

--- src/public/Get-OnBasePercentage.ps1
+++ src/public/Get-OnBasePercentage.ps1
@@ -1,7 +1,22 @@
 
 # TODO: Lets see how you do on this one using lessons from Get-SluggingPercentage
+function Get-OnBasePercentage
+{
+<#
+ .SYNOPSIS
 
-function Get-OnBasePercentage {
+ .DESCRIPTION
+   Long description
+ .EXAMPLE
+   PS C:\> <example usage>
+   Explanation of what the example does
+ .INPUTS
+   Inputs (if any)
+ .OUTPUTS
+   Output (if any)
+ .NOTES
+   General notes
+ #>
 # Hits + Walks + HitsByPitch / PlateAppearances
 param([int] $Hits, [int] $Walks, [int] $HitsByPitch, [int] $PlateAppearances)
 
